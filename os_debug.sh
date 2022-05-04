18f68c4380f24b57a3f3163d20ad2e616ec4d96c
dust1 <834902408@qq.com>
2022-05-04 20:59:59 +0800

    move dir

--- os_debug.sh
+++ os_debug.sh
@@ -1,3 +1,2 @@
-cargo build --release
-rust-objcopy --strip-all ./target/riscv64gc-unknown-none-elf/release/rcore-kernel -O binary ./target/riscv64gc-unknown-none-elf/release/rcore-kernel.bin
-qemu-system-riscv64 -machine virt -nographic -bios bootloader/rustsbi-qemu.bin -device loader,file=target/riscv64gc-unknown-none-elf/release/rcore-kernel.bin,addr=0x80200000 -s -S
+d os && cargo build --release && rust-objcopy --strip-all ./target/riscv64gc-unknown-none-elf/release/rcore-kernel -O binary ./target/riscv64gc-unknown-none-elf/release/rcore-kernel.bin
+cd .. && qemu-system-riscv64 -machine virt -nographic -bios bootloader/rustsbi-qemu.bin -device loader,file=os/target/riscv64gc-unknown-none-elf/release/rcore-kernel.bin,addr=0x80200000 -s -S
